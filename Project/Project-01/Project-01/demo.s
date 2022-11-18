ab3115272adbcdd9a71833d580aa2f6d0e485981
TR-9NF462 <mingxuanz@mail.kakari.cc>
2022-11-17 22:07:31 -0500

    Starting Project 3

--- Project/Project-01/Project-01/demo.s
+++ Project/Project-01/Project-01/demo.s
@@ -1,41 +1,44 @@
-# $1: product
-# $2: multiplicand, also temp
-# $3: multiplier
-# $4: add loop counter, 0 ~ (multiplier - 1)
-# $5: loop flag, break if set to 0
-# $6: bit mask, 0b1 ~ 0b1000000
-# $7: bit loop counter, 0 ~ (7 - 1)
+main:
+    movi $1, 0             # sum
+    movi $2, head         # next_ptr
+    movi $3, 0             # tmp
+    loop:
+        lw $3, 0($2)
+        lw $2, 1($2)
+        add $1, $1, $3
+        jeq $2, $0, done
+        j loop
+    done:
+        halt
 
-movi $1, 0
-lw $2, multiplicand($0)
-lw $3, multiplier($0)
-movi $6, 1
-movi $7, 0
-
-bit_loop:
-
-    movi $4, 0
-
-    add_loop:
-        slt $5, $4, $3                # flag = counter < multiplier
-        jeq $5, $0, add_end            # break if loop counter >= product
-        and $2, $2, $6                # temp ($2) = multiplicand & bitmask
-        add $1, $1, $2                # product += temp
-        lw $2, multiplicand($0)        # restore $2
-        addi $4, $4, 1                 # counter += 1
-        j add_loop
-    add_end:
-
-    slti $5, $7, 7            # flag = counter < 7
-    jeq $5, $0, bit_end        # break if counter >= 7
-    add $6, $6, $6            # left shift by 1 bit
-    addi $7, $7, 1            # counter += 1
-    j bit_loop
-    
-bit_end:
-    halt
-
-multiplicand:
-    .fill 123
-multiplier:
-    .fill 119
+chain4:
+    .fill 100
+    .fill chain5
+    .fill 300
+chain7:
+    .fill 16384
+    .fill 909
+    .fill 0
+chain1:
+    .fill 23
+    .fill chain2
+head:                     # beginning of linked list
+    .fill 34
+    .fill chain1
+    .fill 82
+    .fill 10
+chain3:
+    .fill 4
+    .fill chain4
+    .fill 229
+    .fill 449
+chain2:
+    .fill 0
+    .fill chain3
+chain5:
+    .fill 12
+    .fill 0             # end of linked list
+    .fill 9999
+chain6:
+    .fill 99
+    .fill 49
