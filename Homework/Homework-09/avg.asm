813d9ec8cddc0cb569f60dcf2f38d067d1aa24b8
MacBook <mingxuanz@mail.kakari.cc>
2022-12-09 14:34:24 -0500

    hw9

--- Homework/Homework-09/avg.asm
+++ Homework/Homework-09/avg.asm
@@ -5,4 +5,25 @@ numbers: dd 10, 34, 55, 106, 44, 0, 45, 400
 section .text
 global _start
 _start:
-; Your code here
+
+; eax: sum
+xor eax, eax
+; ebx: cursor
+mov ebx, numbers
+; ecx: count
+mov ecx, 1
+; edx: high 32 bits for dividend, keep 0
+xor edx, edx
+loop:
+    add eax, [ebx]
+    cmp ecx, [numbers_size]
+    je end
+    add ebx, 4
+    inc ecx
+    jmp loop
+end:
+div ecx
+; set return value
+mov ebx, eax
+mov eax, 1
+int 80h
